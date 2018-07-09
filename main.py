2dc9af59b92432e5c5eb0461d6a56e23cdfd37f6
someoneontheinternet <henryjiang0x3639@gmail.com>
2018-07-09 12:59:09 -0700

    Added callback handler

--- main.py
+++ main.py
@@ -3,26 +3,104 @@
 from __future__ import print_function
 from dronekit import connect, VehicleMode
 from datetime import datetime
+import time
 
-def log(msg):
-	print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]: '), end='')
-	print(msg)    
-
+#global variable
 serialRate = 57600
 serialPort = "/dev/ttyS0"
 
-print("Connectin to Hexacopter on device=%s@%d" % (serialPort, serialRate))
+#global functions
+def log(msg, indent = 0):
+	print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]: '), end='')
+	print("\t" * indent, end='')
+	print(msg)
 
+def commands_ready(drone):
+	cmds = drone.commands
+	cmds.download()
+	cmds.wait_ready()
+
+print("Connectin to Hexacopter on device=%s@%d" % (serialPort, serialRate))
 #Setting up connection
 drone = connect(serialPort, wait_ready=True, baud=serialRate)
 
 print("=" * 50)
-
+log("Current Vehicle Status.")
 log("Drone version: %s.%s.%s" % (drone.version.major, drone.version.minor, drone.version.patch))
 log("System Status: %s" % drone.system_status.state)
 log("Current MODE: %s" % drone.mode.name)
 log("Arm Status: %s, is Armable: %s" % (drone.armed, drone.is_armable))
+print("=" * 50)
+log("Current Vehicle Data.")
+log("Attitude: %s" % drone.attitude)
+log("Velocity: %s" % drone.velocity)
+log("GPS: %s" % drone.gps_0)
+log("Battery: %s" % drone.battery)
+log("EKF GYRO Status: %s" % drone.ekf_ok)
+log("Heading: %s" % drone.heading)
+log("Ground Speed: %s" % drone.groundspeed)
+log("%s" % drone.location.global_frame)
 
-log("TESTING: %s" % drone.parameters['RC3_MIN']);
+print("=" * 50)
+
+indent_level = 0
+log("Autopilot Initalization.")
+
+indent_level += 1
+
+#Ensure GPS is functional
+log("GPS Check:", indent_level)
+indent_level += 1
+
+#Getting GPS Status stat >= 3. 3D Lock
+log("Current GPS Status: %s" % drone.gps_0.fix_type, indent_level)
+
+while drone.gps_0.fix_type < 3:
+	log("GPS not fixed: %s" % drone.gps_0.fix_type, indent_level + 1)
+	time.sleep(5)
+log("Current GPS Status: OKAY.", indent_level)
+
+indent_level -= 1
+
+log("Home location:", indent_level)
+#checking for home location
+if not drone.home_location:
+	indent_level += 1
+	log("Home location not found. Using current position. %s" % drone.location.global_frame, indent_level)
+	#Getting current location
+	currentLocation = drone.location.global_frame
+	#Setting current location
+	drone.home_location = currentLocation
+	#Confirmation
+	commands_ready(drone)
+	log("New Location: %s" % drone.home_location, indent_level)
+	indent_level -= 1
+else:
+	log("Pre-existing location: %s" % drone.home_location)
+
+indent_level -= 1
+
+time.sleep(5)
+
+#TODO: Testing Wildcard callback
+
+print("="*50)
+
+# Demonstrate getting callback on any attribute change
+def wildcard_callback(self, attr_name, value):
+	if attr_name == "attitude":
+    		print(" CALLBACK: (%s): %s" % (attr_name,value))
+
+print("\nAdd attribute callback detecting ANY attribute change")     
+drone.add_attribute_listener('*', wildcard_callback)
+
+
+print(" Wait 1s so callback invoked before observer removed")
+time.sleep(5)
+
+print(" Remove Vehicle attribute observer")    
+# Remove observer added with `add_attribute_listener()`
+drone.remove_attribute_listener('*', wildcard_callback)
+
 
 drone.close()
