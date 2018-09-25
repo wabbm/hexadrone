d722994541f3fb9807923e967e89d932bc2d7912
wabbm <40870497+wabbm@users.noreply.github.com>
2018-09-25 15:34:21 -0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -179,7 +179,7 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
     """
     Move vehicle in direction based on specified velocity vectors.
     """
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+    msg = vehicle.message_factory.set_position_target_local_ned_encode( 
         0,       # time_boot_ms (not used)
         0, 0,    # target system, target component
         mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
